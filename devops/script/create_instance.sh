a68021589f599b39ee30c225652053909c357091
tilakmishra <tilak.mishra@gmail.com>
2016-11-26 04:05:08 +0000

    added access key

--- devops/script/create_instance.sh
+++ devops/script/create_instance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-knife ec2 server create --region=us-east-1 --subnet  subnet-c1ee9eb6 -I ami-16f26201 --flavor=t2.micro -r 'role[prod]' -E prod --node-ssl-verify-mode none -N devopstesting -S devopsnew --ssh-user ubuntu --identity-file /home/ubuntu/devopsnew.pem --security-group-ids sg-e4af3099 --server-connect-attribute public_ip_address
+knife ec2 server create --aws-access-key-id AKIAJ7DZHMJITUYHMEOQ  --aws-secret-access-key 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 --region=us-east-1 --subnet  subnet-c1ee9eb6 -I ami-16f26201 --flavor=t2.micro -r 'role[prod]' -E prod --node-ssl-verify-mode none -N devopstesting -S devopsnew --ssh-user ubuntu --identity-file /home/ubuntu/devopsnew.pem --security-group-ids sg-e4af3099 --server-connect-attribute public_ip_address
