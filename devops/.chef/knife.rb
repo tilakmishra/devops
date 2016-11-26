fe51264198ac878e5889c49a0dc137dacbd3d340
tilakmishra <tilak.mishra@gmail.com>
2016-11-26 04:17:54 +0000

    added content

--- devops/.chef/knife.rb
+++ devops/.chef/knife.rb
@@ -1,11 +1,11 @@
 log_level                :info
 log_location             STDOUT
 node_name                'admin'
-client_key               '/home/ubuntu/devops/.chef/admin.pem'
+client_key               '/var/lib/jenkins/workspace/devops_testing/devops/.chef/admin.pem'
 validation_client_name   'chef-validator'
-validation_key           '/home/ubuntu/devops/.chef/chef-validator.pem'
+validation_key           '/var/lib/jenkins/workspace/devops_testing/devops/.chef/chef-validator.pem'
 chef_server_url          'https://54.197.196.38:443'
-syntax_check_cache_path  '/home/ubuntu/devops/.chef/syntax_check_cache'
+syntax_check_cache_path  '/var/lib/jenkins/workspace/devops_testing/devops/.chef/syntax_check_cache'
 ssl_verify_mode    :verify_none
 knife[:aws_access_key_id] = "AKIAJ7DZHMJITUYHMEOQ"
 knife[:aws_secret_access_key] = "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"
