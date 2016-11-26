8a6bdf176d71a553b845f09fab10d91bcd9d7ddc
tilakmishra <tilak.mishra@gmail.com>
2016-11-26 03:51:13 +0000

    added script

---
+++ script/create_instance.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+knife ec2 server create --region=us-east-1 --subnet  subnet-c1ee9eb6 -I ami-16f26201 --flavor=t2.micro -r 'role[prod]' -E prod --node-ssl-verify-mode none -N devopstesting -S devopsnew --ssh-user ubuntu --identity-file /home/ubuntu/devopsnew.pem --security-group-ids sg-e4af3099 --server-connect-attribute public_ip_address --node-ssl-verify-mode none`
